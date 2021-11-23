25654d69d10cf1048aea2bd4c26103ce178c89ae
Kim Coleman <kim@strangerstudios.com>
2021-11-23 08:12:49 -0500

    Updating doc block

--- admin/class-convertkit-pmp-admin.php
+++ admin/class-convertkit-pmp-admin.php
@@ -487,6 +487,7 @@ class ConvertKit_PMP_Admin {
 			 * Allow custom code to add additional fields for the subscriber.
 			 *
 			 * @param array $subscribe_fields The array of fields to add for the subscriber.
+			 * @param string $user_email The user's email address to subscribe tags for.
 			 *
 			 * @return array The array of fields to add for the subscriber.
 			 */
