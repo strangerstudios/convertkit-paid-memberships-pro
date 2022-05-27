d41642775b2ff6f93754dc315b1e27e066a227f4
Jarryd Long <JarrydLong@users.noreply.github.com>
2022-05-27 13:30:09 +0200

    Update subscriber details upon saving user profile

--- admin/class-convertkit-pmp-admin.php
+++ admin/class-convertkit-pmp-admin.php
@@ -634,6 +634,44 @@ class ConvertKit_PMP_Admin {
 		}
 	}
 
+	/**		
+ 	 * Updates a subscriber's details upon updating a user profile in wp-admin		
+ 	 *		
+ 	 * @access public		
+ 	 * @since TBD		
+ 	 * @return void		
+ 	 */		
+ 	public function update_profile( $user_id ) {	
+
+	 	if ( current_user_can( 'edit_user', $user_id ) ) {
+
+			$user_id = isset( $_REQUEST['user_id'] ) ? intval( $_REQUEST['user_id'] ) : 0;
+
+			if( !empty( $user_id ) ) {
+
+				$user_email = isset( $_REQUEST['email'] ) ? sanitize_email( $_REQUEST['email'] ) : '';
+				$first_name = isset( $_REQUEST['first_name'] ) ? sanitize_text_field( $_REQUEST['first_name'] ) : '';
+
+				$subscriber_id = get_user_meta( $user_id, 'pmprock_subscriber_id', true );
+
+				$subscriber_info = array(
+					'email_address' 	=> $user_email,
+					'first_name' 		=> $first_name,
+					'user_id' 			=> $user_id
+				);
+
+				$subscriber_info = apply_filters( 'pmprock_subscriber_update_data', $subscriber );
+
+				// Get the secret API key.
+				$api_secret_key = $this->get_option( 'api-secret-key' );
+
+				$this->api->update_subscriber( $subscriber_id, $api_secret_key, $subscriber_info );
+			}
+
+	 	}
+
+	}
+
 	/**
 	 * Get the setting option requested.
 	 *
